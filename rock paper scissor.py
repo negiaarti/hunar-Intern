0a6abd0f36c221e9af53a2d00fe979a84829cb5f
negiaarti <negi.sarita030@gmail.com>
2024-06-27 18:55:53 +0530

    Add files via upload

---
+++ rock paper scissor.py
@@ -0,0 +1,65 @@
+import random
+
+def get_computer_choice():
+    choices = ["rock", "paper", "scissors"]
+    return random.choice(choices)
+
+def determine_winner(user_choice, computer_choice):
+    if user_choice == computer_choice:
+        return "It's a draw!"
+    elif (user_choice == "rock" and computer_choice == "scissors") or \
+         (user_choice == "scissors" and computer_choice == "paper") or \
+         (user_choice == "paper" and computer_choice == "rock"):
+        return "You win!"
+    else:
+        return "You lose!"
+
+def play_game():
+    user_choice = input("Enter rock, paper, or scissors: ").lower()
+    while user_choice not in ["rock", "paper", "scissors"]:
+        user_choice = input("Invalid choice. Enter rock, paper, or scissors: ").lower()
+    
+    computer_choice = get_computer_choice()
+    print(f"Computer chose: {computer_choice}")
+    
+    result = determine_winner(user_choice, computer_choice)
+    print(result)
+
+if __name__ == "__main__":
+    play_game()
+
+import random
+
+def get_computer_choice():
+    choices = ["rock", "paper", "scissors"]
+    return random.choice(choices)
+
+def determine_winner(user_choice, computer_choice):
+    if user_choice == computer_choice:
+        return "It's a draw!"
+    elif (user_choice == "rock" and computer_choice == "scissors") or \
+         (user_choice == "scissors" and computer_choice == "paper") or \
+         (user_choice == "paper" and computer_choice == "rock"):
+        return "You win!"
+    else:
+        return "You lose!"
+
+def play_game():
+    while True:
+        user_choice = input("Enter rock, paper, or scissors: ").lower()
+        while user_choice not in ["rock", "paper", "scissors"]:
+            user_choice = input("Invalid choice. Enter rock, paper, or scissors: ").lower()
+        
+        computer_choice = get_computer_choice()
+        print(f"Computer chose: {computer_choice}")
+        
+        result = determine_winner(user_choice, computer_choice)
+        print(result)
+        
+        play_again = input("Do you want to play again? (yes/no): ").lower()
+        if play_again != 'yes':
+            print("Thanks for playing!")
+            break
+
+if __name__ == "__main__":
+    play_game()
---
+++ rock paper.py
@@ -0,0 +1,14 @@
+Python 3.12.4 (tags/v3.12.4:8e8a4ba, Jun  6 2024, 19:30:16) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/aarti/AppData/Local/Programs/Python/Python312/rock paper scissor.py
+Enter rock, paper, or scissors: scissors
+Computer chose: paper
+You win!
+Enter rock, paper, or scissors: rock
+Computer chose: scissors
+You win!
+Do you want to play again? (yes/no): yes
+Enter rock, paper, or scissors: paper
+Computer chose: paper
+It's a draw!
